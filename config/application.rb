e820d1fae4b6699ba9b8f0bb5eb389be60ede916
Frank <fv42wid@yahoo.com>
2014-10-03 16:19:20 -0400

    added welcome.css to assests.precompile

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ Bundler.require(*Rails.groups)
 module Vibe
   class Application < Rails::Application
 
+    config.assets.precompile += [
+      'welcome.css'
+    ]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
